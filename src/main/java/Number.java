da673c9b4969e6da630dcbee5f1a700effcde133
kbsssudha <kbssudha@gmail.com>
2024-01-01 21:36:22 +0530

    Number

---
+++ src/main/java/Number.java
@@ -0,0 +1,11 @@
+public class Number {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        int c;
+        c=a+b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
